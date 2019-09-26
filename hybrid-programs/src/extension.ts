cf83c507afccc8dfd7578012d325ea0d0521d29d
zstone <zstonex@gmail.com>
2019-09-26 19:53:44 -0400

    cleaning up still

--- hybrid-programs/src/extension.ts
+++ hybrid-programs/src/extension.ts
@@ -1,15 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-console.log('did something');
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from 'vscode-languageclient';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+   //let client = LanguageClient.foo;
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hybrid-programs" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
